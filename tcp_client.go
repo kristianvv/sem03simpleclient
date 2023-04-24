ecd3ec2a97d33400b0774d6d6215eba5b474a90c
kristianvv <kristianvaag@gmail.com>
2023-04-24 08:55:29 +0000

    opp

--- tcp_client.go
+++ tcp_client.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+
+	"github.com/kristianvv/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+	defer conn.Close()
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	encrypted := mycrypt.Encrypt([]byte(os.Args[1]))
+	_, err = conn.Write(encrypted)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+
+	decrypted := mycrypt.Decrypt(buf[:n])
+	response := string(decrypted)
+	log.Printf("reply from server: %s", response)
 }
