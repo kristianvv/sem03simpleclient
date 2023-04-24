2bd9a952a1ed81f69c150eb688a1418edc232513
kristianvv <kristianvaag@gmail.com>
2023-04-24 10:26:09 +0000

    update

--- tcp_client.go
+++ tcp_client.go
@@ -4,12 +4,39 @@ import (
 	"log"
 	"net"
 	"os"
-
-	"github.com/kristianvv/is105sem03/mycrypt"
 )
 
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789 .,:;")
+
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+	kryptertMelding := make([]rune, len(melding))
+	for i := 0; i < len(melding); i++ {
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		if indeks == -1 {
+			kryptertMelding[i] = melding[i]
+			continue
+		}
+		if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		} else {
+			kryptertMelding[i] = alphabet[indeks+chiffer]
+		}
+	}
+	return kryptertMelding
+}
+
+func sokIAlfabetet(symbol rune, alfabet []rune) int {
+	for i := 0; i < len(alfabet); i++ {
+		if symbol == alfabet[i] {
+			return i
+			break
+		}
+	}
+	return -1
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:8080")
+	conn, err := net.Dial("tcp", "172.17.0.2:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,8 +44,9 @@ func main() {
 
 	log.Println("os.Args[1] = ", os.Args[1])
 
-	encrypted := mycrypt.Encrypt([]byte(os.Args[1]))
-	_, err = conn.Write(encrypted)
+	message := []rune(os.Args[1])
+	encrypted := Krypter(message, ALF_SEM03, 5)
+	_, err = conn.Write([]byte(string(encrypted)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,7 +57,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	decrypted := mycrypt.Decrypt(buf[:n])
-	response := string(decrypted)
-	log.Printf("reply from server: %s", response)
+	response := string(buf[:n])
+	decrypted := Krypter([]rune(response), ALF_SEM03, -5)
+	log.Printf("reply from server: %s", string(decrypted))
 }
