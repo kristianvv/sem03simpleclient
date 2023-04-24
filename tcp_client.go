ad385c7a741aab129f8520ed67b5494ead7d4e1c
kristianvv <kristianvaag@gmail.com>
2023-04-24 13:24:34 +0000

    oppdaterte client

--- tcp_client.go
+++ tcp_client.go
@@ -29,7 +29,7 @@ func main() {
 	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
-	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)+4)
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 	log.Println("Dekrypter melding: ", string(dekryptertMelding))
 
 }
